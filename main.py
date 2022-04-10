32f4cdf638723fdaee787738e5b668d873567165
Nextross <70912486+Nextross@users.noreply.github.com>
2022-04-10 13:02:04 +0200

    Add files via upload
    Opraven bug, který mazal nuly z dat, i když to nebylo potřeba.

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 import requests
 import json
+import os
+import sys
 from notifypy import Notify
 from time import sleep
-from sys import exit
 
-# sleep(10)
+sleep(10)
+
+
+# Pouze pro převod .py na .exe
+def resource_path(relative_path):
+    try:
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
 
 
 def json_to_dic(json_object):
@@ -32,16 +43,16 @@ def todays_name_day():
                 notification.title = f"Nepodařilo se připojit k serveru"
                 notification.message = f"Ukončování aplikace..."
                 notification.send()
-                exit()
+                sys.exit()
 
     response = json_to_dic(response)
     name_day = response["name"]
     day = response["date"]
 
-    notification.title = f"Dnes {day[:2].strip('0')}. {day[2:].strip('0')}"
+    notification.title = f"Dnes {day[:2].lstrip('0')}. {day[2:].lstrip('0')}."  # 10. 4. -> 1. 4.
     notification.message = f"má svátek {name_day}"
     notification.send()
 
 
-notification = Notify(default_notification_application_name="Kdo má dnes svátek?", default_notification_icon="icon.ico")
+notification = Notify(default_notification_application_name="Kdo má dnes svátek?", default_notification_icon=resource_path("icon.ico"))
 todays_name_day()
